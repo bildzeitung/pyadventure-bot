538d16e8c92670f621647e9d5b7999a8c541b875
daniel.klein <daniel.klein@oracle.com>
2016-06-29 23:37:54 -0400

    Remove vcversioner

--- scottadams/setup.py
+++ scottadams/setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import os
-
 from setuptools import setup, find_packages
 
 setup(
@@ -12,16 +10,11 @@ setup(
     url='http://casa.blan.ca',
     packages=find_packages(exclude=["tests"]),
     test_suite='nose.collector',
-    setup_requires=['vcversioner'],
     install_requires=[
         'click',
     ],
     tests_require=[
     ],
-    vcversioner={
-        'root': os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'),
-        'version_module_paths': ['lib/_version.py'],
-    },
     entry_points={
         'console_scripts': ['adventure=cli:main'],
     },
