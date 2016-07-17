2a13670c7ed7f100fc9ade78cfa125e57a98a870
daniel.klein <daniel.klein@oracle.com>
2016-07-16 22:53:32 -0400

    Updates postgres makefile to be more strictly true

--- mk/postgres.mk
+++ mk/postgres.mk
@@ -18,27 +18,46 @@ ${PG_DATA}/postgresql.conf:
 	mkdir -p ${PG_DATA}
 	${PG_PATH}/initdb -D ${PG_DATA} -E UNICODE
 
-${PG_SOCKET}:
+${PG_SOCKET}: ${PG_DATA}/postgresql.conf
 	mkdir -p ${PG_RUN}
 	mkdir -p ${PG_LOG}
 	${PG_PATH}/pg_ctl $(PG_PARM) start -w
 
-${PG_DATA}/init:
+${PG_DATA}/init: ${PG_SOCKET}
 	${PG_PATH}/psql -h ${PG_RUN} -d postgres -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PWD}';"
 	${PG_PATH}/createdb -E UTF8 ${DB_NAME} -h ${PG_RUN} -O ${DB_USER}
-	echo 1 > ${PG_DATA}/init
+	touch ${PG_DATA}/init
 
-db_start: ${PG_DATA}/postgresql.conf ${PG_SOCKET} ${PG_DATA}/init
+#
+# startup postgres daemon
+#
+db_start: ${PG_DATA}/init
 
+# 
+# shutdown postgres daemon
+#
 db_stop:
 	test -f ${PG_DATA}/postmaster.pid && ${PG_PATH}/pg_ctl $(PG_PARM) stop -m i || true
 
+#
+# check status of database
+#
 db_status:
-	${PG_PATH}/pg_ctl $(PG_PARM) status
+	@if [ -d "${PG_DIR}" ]; then \
+		${PG_PATH}/pg_ctl $(PG_PARM) status ; \
+	else \
+		echo Database directory not yet made ; \
+	fi
 
+#
+# shell in to db
+#
 db_dev: ${PG_SOCKET}
 	psql -d postgres://${DB_USER}:${DB_PWD}@localhost:${PG_PORT}/${DB_NAME}
 
+#
+# remove database files (shutting the daemon down if necessary)
+#
 db_clean: db_stop
 	rm -fr ${PG_DIR}
 
